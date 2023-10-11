395b848345cc51d5c3b5b4b8fccf5a459fe9e550
Nattan <nleguyader@gaming.tech>
2023-10-11 20:33:51 +0200

    gdf

--- public/voiture.js
+++ public/voiture.js
@@ -1,18 +1,13 @@
-const test = async () => {
-    console.log('A');
-    const splineEUID = "3bb55065-4c5a-4507-8ba6-1d80a9df1f0c";
-    console.log('B');
-    const voitureEUID = "1f9c1d9a-338d-4eaf-8adb-02d21653befb";
-    console.log('C');
-    const spline = await SDK3DVerse.engineAPI.findEntitiesByEUID("3bb55065-4c5a-4507-8ba6-1d80a9df1f0c");
+var test = async () => {
+    var spline = await SDK3DVerse.engineAPI.findEntitiesByEUID("3bb55065-4c5a-4507-8ba6-1d80a9df1f0c");
     console.log('D');
-    const voiture = await SDK3DVerse.engineAPI.findEntitiesByEUID("1f9c1d9a-338d-4eaf-8adb-02d21653befb");
+    var voiture = await SDK3DVerse.engineAPI.findEntitiesByEUID("1f9c1d9a-338d-4eaf-8adb-02d21653befb");
     console.log('E');
     console.log('Spline: ', spline);
     console.log('Voiture: ', voiture);
     console.log('debug children: ', SDK3DVerse.engineAPI.getEntityChildren(spline));
 
-    const trajEntities = spline[0];
+    var trajEntities = spline[0];
     let allpoints = SDK3DVerse.engineAPI.getEntityChildren(trajEntities);
 
     let allpositions = new Array();
