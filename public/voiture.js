539989c0f62064f6ff29f6a05b330b9b7ac82285
Nattan <nleguyader@gaming.tech>
2023-10-11 18:24:47 +0200

    hjv

--- public/voiture.js
+++ public/voiture.js
@@ -2,6 +2,7 @@ let voiture = SDK3DVerse.engineAPI.findEntitiesByEUID("1f9c1d9a-338d-4eaf-8adb-0
 let spline = SDK3DVerse.engineAPI.findEntitiesByEUID("3bb55065-4c5a-4507-8ba6-1d80a9df1f0c");
 //let splineName = spline.getName();
 let allpoints = SDK3DVerse.engineAPI.getEntityChildren(spline);
+console.log('Spline: ', SDK3DVerse.engineAPI.findEntitiesByEUID("3bb55065-4c5a-4507-8ba6-1d80a9df1f0c"));
 console.log('debug children: ', SDK3DVerse.engineAPI.getEntityChildren(spline));
 let allpositions = new Array();
 allpoints.forEach(point => {
