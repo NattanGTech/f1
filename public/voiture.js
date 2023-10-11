1d24f9658340f50f7d830c3beedf4a25f896eb8e
Nattan <nleguyader@gaming.tech>
2023-10-11 20:38:11 +0200

    dhgd

--- public/voiture.js
+++ public/voiture.js
@@ -1,27 +1,32 @@
-var test = async () => {
-    var spline = await SDK3DVerse.engineAPI.findEntitiesByEUID("3bb55065-4c5a-4507-8ba6-1d80a9df1f0c");
-    console.log('D');
-    var voiture = await SDK3DVerse.engineAPI.findEntitiesByEUID("1f9c1d9a-338d-4eaf-8adb-02d21653befb");
-    console.log('E');
-    console.log('Spline: ', spline);
-    console.log('Voiture: ', voiture);
-    console.log('debug children: ', SDK3DVerse.engineAPI.getEntityChildren(spline));
-
-    var trajEntities = spline[0];
-    let allpoints = SDK3DVerse.engineAPI.getEntityChildren(trajEntities);
-
-    let allpositions = new Array();
-    allpoints.forEach(point => {
-        allpositions.push(point.getGlobalTransform());
+function GameLoop() { 
+    window.requestAnimationFrame(function() {
+        voiture.setPosition(allpositions[i]);
+        i++;
+        SDK3DVerse.engineAPI.propagateChanges();
+        GameLoop();
     });
-    function GameLoop() { 
-        window.requestAnimationFrame(function() {
-            voiture.setPosition(allpositions[i]);
-            i++;
-            SDK3DVerse.engineAPI.propagateChanges();
-            GameLoop();
-        });
-    }
-    GameLoop();
 }
-test();
+
+canvas.addEventListener(
+    'mouseup',
+    async (e) =>
+    {
+        var spline = await SDK3DVerse.engineAPI.findEntitiesByEUID("3bb55065-4c5a-4507-8ba6-1d80a9df1f0c");
+        console.log('D');
+        var voiture = await SDK3DVerse.engineAPI.findEntitiesByEUID("1f9c1d9a-338d-4eaf-8adb-02d21653befb");
+        console.log('E');
+        console.log('Spline: ', spline);
+        console.log('Voiture: ', voiture);
+        console.log('debug children: ', SDK3DVerse.engineAPI.getEntityChildren(spline));
+
+        var trajEntities = spline[0];
+        let allpoints = SDK3DVerse.engineAPI.getEntityChildren(trajEntities);
+
+        let allpositions = new Array();
+        allpoints.forEach(point => {
+            allpositions.push(point.getGlobalTransform());
+        });
+        GameLoop();
+    },
+    false
+);
