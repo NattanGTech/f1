181cd42f423a8114e4171445037e4659eb082885
Nattan <nleguyader@gaming.tech>
2023-10-11 18:53:31 +0200

    hgfh

--- public/voiture.js
+++ public/voiture.js
@@ -4,9 +4,9 @@ const test = async () => {
     console.log('B');
     const voitureEUID = "1f9c1d9a-338d-4eaf-8adb-02d21653befb";
     console.log('C');
-    const spline = await SDK3DVerse.engineAPI.findEntitiesByEUID(splineEUID.toString());
+    const spline = await SDK3DVerse.engineAPI.findEntitiesByEUID("3bb55065-4c5a-4507-8ba6-1d80a9df1f0c");
     console.log('D');
-    const voiture = await SDK3DVerse.engineAPI.findEntitiesByEUID(voitureEUID.toString());
+    const voiture = await SDK3DVerse.engineAPI.findEntitiesByEUID("1f9c1d9a-338d-4eaf-8adb-02d21653befb");
     console.log('E');
     console.log('Spline: ', spline);
     console.log('Voiture: ', voiture);
