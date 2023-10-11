3aa2494166d0ccd8a25c6ecc89b90d239de373fd
Nattan <nleguyader@gaming.tech>
2023-10-11 18:43:18 +0200

    dthfgh

--- public/voiture.js
+++ public/voiture.js
@@ -1,8 +1,13 @@
 const test = async () => {
+    console.log('A');
     const splineEUID = "3bb55065-4c5a-4507-8ba6-1d80a9df1f0c";
+    console.log('B');
     const voitureEUID = "1f9c1d9a-338d-4eaf-8adb-02d21653befb";
+    console.log('C');
     const spline = await SDK3DVerse.engineAPI.findEntitiesByEUID(splineEUID);
+    console.log('D');
     const voiture = await SDK3DVerse.engineAPI.findEntitiesByEUID(voitureEUID);
+    console.log('E');
     console.log('Spline: ', spline);
     console.log('Voiture: ', voiture);
     console.log('debug children: ', SDK3DVerse.engineAPI.getEntityChildren(spline));
